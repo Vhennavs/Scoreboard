a3c2447d25e014b56a16bbb70f262bc574d4a1ba
Vhennavs <147713832+Vhennavs@users.noreply.github.com>
2023-10-27 17:45:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
-let count = 0
+
 
 function increment() {
-    count += 1
-    countEl.textContent = count
+   saveEl.textContent = document.getElementById ("profile-btn")
 }
-
-function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
-    count = 0
-}
-
-function iloveyou()
-
-{
- {console.log" i love you margie"
-saveEl.textContent(iloveyou())}
-}
-iloveyou()
+increment()
